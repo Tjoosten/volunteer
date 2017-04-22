3ce23374e182e8f0509eaafc296b81359b9cdc2d
Tim Joosten <Tjoosten3@gmail.com>
2017-04-23 01:19:00 +0200

    Implement the controller

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -1,7 +1,15 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
+/**
+ * @todo docblock
+ */
 class Account extends CI_Controller
 {
+    public $user        = []; /** TODO: docblock */
+    public $permissions = []; /** TODO: docblock */
+    public $abilities   = []; /** TODO: docblock */
+    public $language    = []; /** TODO: docblock */
+
     /**
      * Account constructor.
      *
@@ -10,28 +18,63 @@ class Account extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-    }
+        $this->load->library(['blade', 'form_validation', 'session']);
+        $this->load->helper(['url']);
 
-    public function index()
-    {
-        $data['title'] = '';
-        return $this->blade->render('', $data);
+        $this->user        = $this->session->userdata('user');
+        $this->permissions = $this->session->userdata('permissions');
+        $this->abilities   = $this->session->userdata('abilities');
+        $this->language    = $this->session->userdata('language');
     }
 
     /**
+     * Get the profile settings page.
+     *
+     * @see:url('GET|HEAD', 'http://www.vrijwilligers.activisme.be/account')
+     * @return Blade View
+     */
+    public function index()
+    {
+        $data['title'] = $this->lang->line('title-account');
+        return $this->blade->render('auth/profile', $data);
+    }
+
+    /**
+     * Update the account information in the database.s
      *
      * @see:url('POST', 'http://www.vrijwilligers.activisme.be/account/updateSettings')
      * @return Blade view | Response
      */
     public function updateSettings()
     {
+        $this->form_validation->set_rules('name', 'Name', 'trim|required');
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[users.username]');
+        $this->form_validation->set_rules('email', 'Email Address', 'trim|required|is_unique[users.email]');
 
+        if ($this->form_validation->run() === false) { // Form validation fails.
+            $data['user']  = Authencate::find($this->user['id']);
+            $data['title'] = $this->lang->line('title-account');
+
+            return $this->blade->render('auth/profile');
+        }
+
+        // Inputs
+        $input['name']     = $this->input->post('name');
+        $input['username'] = $this->input->post('username');
+        $input['email']    = $this->input->post('email');
+
+        if ($user = Authencate::find($this->user['id'])->update($this->security->xss_clean($input))) {
+            $this->session->set_flashdata('class', 'alert alert-success');
+            $this->session->set_flashdata('message', $this->lang->line('flash-update-account'));
+        }
+
+        return redirect($_SERVER['HTTP_REFERER']);
     }
 
     /**
      * Update the user his password.
      *
-     * @see:url('POST', '')
+     * @see:url('POST', 'http://www.vrijwilligers.activisme.be/account/updatePassword')
      * @return Blade view | Response
      */
     public function updatePassword()
@@ -48,7 +91,7 @@ class Account extends CI_Controller
 
         $input['password'] = $this->input->post('paassword');
 
-        if ($user = Authencation::find($this->security->xss_clean($input))) {
+        if ($user = Authencation::find($this->user['id'])->update($this->security->xss_clean($input))) {
             $this->session->set_flashdata('class', 'alert alert-success');
             $this->session->set_flashdata('message', $this->lang->line('flash-update-password'));
         }
---
+++ application/views/auth/profile.blade.php
@@ -0,0 +1,8 @@
+@layout('layouts/app')
+
+@section('content')
+    <div class="container padding-container">
+        <div class="row">
+        </div>
+    </div>
+@endsection
