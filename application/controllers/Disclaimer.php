69b72ca8495dcc709e4c637e3d5cacbae4d6bf50
Tim Joosten <Tjoosten3@gmail.com>
2017-04-23 22:08:46 +0200

    Implement disclaimer controller

---
+++ application/controllers/Disclaimer.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @todo docblock
+ */
+class Disclaimer extends CI_Controller
+{
+    public $user        = []; /** @var array|mixed */
+    public $permissions = []; /** @var array|mixed */
+    public $abilities   = []; /** @var array|mixed */
+    public $language    = []; /** @var array|mixed */
+
+    /**
+     * Disclaimer constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['blade', 'session']);
+        $this->load->helper(['url']);
+
+        $this->user        = $this->session->set_flashdata('user');
+        $this->permissions = $this->session->set_flashdata('permissions');
+        $this->abilities   = $this->session->set_flashdata('abilities');
+        $this->language    = $this->session->set_flashdata('language');
+    }
+
+    /**
+     * Show the disclaimer for the platform.
+     *
+     * @see:url('GET|HEAD', 'http://www.vrijwilligers.activisme.be/disclaimer')
+     * @return Blade view
+     */
+    public function index()
+    {
+        return $this->blade->render('disclaimer', ['title' => 'Disclaimer']);
+    }
+}
